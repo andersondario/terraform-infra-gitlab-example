4ea80876f2bd8de49207fce0ae2498823fff10fd
Anderson Dario Arendt <andersondario@gmail.com>
2023-01-14 22:29:04 +0000

    Dev Changes

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -4,7 +4,7 @@ terraform {
   }
 
   backend "gcs" {
-    bucket = ""
+    bucket = "ad-terraform-live-state-dev"
     prefix = "terraform/state"
   }
 }
@@ -15,25 +15,6 @@ provider "google" {
 }
 
 locals {
-  firewall_rules = [{}]
-  project_id     = "aa-env-dev"
-  environment    = "dev"
-}
-
-module "firewall" {
-  source = "../../modules/gcp/firewall-rule"
-  for_each = {
-    for index, rule in local.firewall_rules : rule.name => rule
-  }
-
-  project     = local.project_id
-  environment = local.environment
-
-  name              = each.value.name
-  description       = each.value.description
-  network_name      = each.value.network_name
-  allowed_tcp_ports = each.value.allowed_tcp_ports
-  source_tags       = each.value.source_tags
-  source_ranges     = each.value.source_ranges
-  target_tags       = each.value.target_tags
+  project_id  = "aa-env-dev"
+  environment = "dev"
 }
